b167815d30a7c219ff708b372db06b1a4e113413
Prateek Patel <145681959+Prateekcodex@users.noreply.github.com>
2026-08-15 20:02:25 +0530

    Time: 1 ms (96.36%), Space: 44 MB (44.11%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+
+        List<Boolean> result = new ArrayList<>();
+
+        int max = 0;
+
+        // Find the maximum candies
+        for (int candy : candies) {
+            max = Math.max(max, candy);
+        }
+
+        // Check each kid
+        for (int candy : candies) {
+            result.add(candy + extraCandies >= max);
+        }
+
+        return result;
+    }
+}
