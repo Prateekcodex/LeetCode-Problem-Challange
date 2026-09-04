8dd26f1b4df80ad7f7256620db40beaf1607fc5d
Prateek Patel <145681959+Prateekcodex@users.noreply.github.com>
2026-09-04 19:16:43 +0530

    Time: 1 ms (99.93%), Space: 46.2 MB (92.1%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+
+        boolean[][] rows = new boolean[9][9];
+        boolean[][] cols = new boolean[9][9];
+        boolean[][] boxes = new boolean[9][9];
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+
+                if (board[i][j] == '.') {
+                    continue;
+                }
+
+                int num = board[i][j] - '1';
+
+                // Find 3x3 box number
+                int box = (i / 3) * 3 + (j / 3);
+
+                // Duplicate found
+                if (rows[i][num] || cols[j][num] || boxes[box][num]) {
+                    return false;
+                }
+
+                // Mark as used
+                rows[i][num] = true;
+                cols[j][num] = true;
+                boxes[box][num] = true;
+            }
+        }
+
+        return true;
+    }
+}
